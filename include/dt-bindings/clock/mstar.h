727ef9f65f096927fb54931d03ecc4e3dae7c381
Daniel Palmer <daniel@0x0f.com>
2020-08-22 23:35:33 +0900

    ARM: mstar: mpll

---
+++ include/dt-bindings/clock/mstar.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _DT_BINDINGS_CLOCK_MSTAR_H
+#define _DT_BINDINGS_CLOCK_MSTAR_H
+
+#define MSTAR_MPLL_432			0
+#define MSTAR_MPLL_345			1
+#define MSTAR_MPLL_288			2
+#define MSTAR_MPLL_216			3
+#define MSTAR_MPLL_172			4
+#define MSTAR_MPLL_144			5
+#define MSTAR_MPLL_123			6
+#define MSTAR_MPLL_124			7
+#define MSTAR_MPLL_86			8
+
+#endif
