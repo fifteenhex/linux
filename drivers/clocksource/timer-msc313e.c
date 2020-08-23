506a315262a98daa47329d61c3a787e503971834
Daniel Palmer <daniel@0x0f.com>
2020-08-23 18:56:22 +0900

    timer cleanup

--- drivers/clocksource/timer-msc313e.c
+++ drivers/clocksource/timer-msc313e.c
@@ -1,139 +1,193 @@
-#include <linux/module.h>
-#include <linux/of.h>
-#include <linux/of_irq.h>
-#include <linux/platform_device.h>
 #include <linux/clk.h>
-#include <linux/clocksource.h>
-#include <asm-generic/io.h>
+#include <linux/clockchips.h>
 #include <linux/interrupt.h>
-#include <linux/regmap.h>
+#include <linux/irq.h>
+#include <linux/irqreturn.h>
+#include <linux/sched_clock.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
 
-/*
- * 0x0	- ctrl
- *  8  |   4     |   3   |  1   |  0
- * int | capture | clear | trig |  ~en
- * 
- * 0x08 - max low
- * 0x0c - max high
- * 0x10 - counter low word
- * 0x14 - counter high word
- * 
- */
+#include "timer-of.h"
 
-#define DRIVER_NAME "msc313e-timer"
-#define REG_CTRL         0
-#define REG_COUNTER_LOW  0x10
-#define REG_COUNTER_HIGH 0x14
+#define REG_CTRL		0
+#define CTRL_ENABLE		BIT(0)
+#define CTRL_TRIG		BIT(1)
+#define CTRL_CLEAR		BIT(2)
+#define CTRL_CAPTURE		BIT(3)
+#define CTRL_IRQ		BIT(8)
+#define REG_COMPARE_LOW		0x8
+#define REG_COMPARE_HIGH	0xc
+#define REG_COUNTER_LOW		0x10
+#define REG_COUNTER_HIGH	0x14
+
+#define TIMER_SYNC_TICKS	3
 
 struct msc313e_timer {
-	__iomem void* base;
-	struct clk *clk;
+	struct timer_of oftimer;
 	struct clocksource clksrc;
-	struct regmap *regmap;
-	struct regmap_field* noten;
 };
+
 #define to_msc313e_timer(ptr) container_of(ptr, struct msc313e_timer, clksrc)
 
-static const struct of_device_id msc313e_timer_dt_ids[] = {
-	{ .compatible = "mstar,msc313e-timer" },
-	{},
-};
-MODULE_DEVICE_TABLE(of, msc313e_timer_dt_ids);
-
-static const struct regmap_config msc313_timer_regmap_config = {
-		.reg_bits = 16,
-		.val_bits = 16,
-		.reg_stride = 4,
-};
-
-static struct reg_field noten_field = REG_FIELD(REG_CTRL, 0, 0);
-
-static irqreturn_t msc313e_timer_irq(int irq, void *data)
+static int msc313e_timer_clkevt_shutdown(struct clock_event_device *evt)
 {
+	struct timer_of *timer = to_timer_of(evt);
+	u16 reg;
+
+	printk("shutdown\n");
+
+	reg = readw_relaxed(timer->of_base.base + REG_CTRL);
+	reg &= ~CTRL_ENABLE;
+	writew_relaxed(reg, timer->of_base.base + REG_CTRL);
+
+	return 0;
+}
+
+static int msc313e_timer_clkevt_set_oneshot(struct clock_event_device *evt)
+{
+	struct timer_of *timer = to_timer_of(evt);
+	u16 reg = 0;
+
+	printk("one shot\n");
+
+	//result = readw_relaxed(timer->oftimer.of_base.base + REG_COUNTER_LOW);
+
+	return 0;
+}
+
+static int msc313e_timer_set_periodic(struct clock_event_device *evt)
+{
+	struct timer_of *timer = to_timer_of(evt);
+
+	printk("periodic\n");
+
+	writew_relaxed(CTRL_ENABLE | CTRL_IRQ,
+			timer->of_base.base + REG_CTRL);
+	return 0;
+}
+
+static int msc313e_timer_clkevt_next_event(unsigned long evt,
+				   struct clock_event_device *clkevt)
+{
+	struct timer_of *to = to_timer_of(clkevt);
+
+	printk("next\n");
+
+	return 0;
+}
+
+
+static irqreturn_t msc313e_timer_clkevt_irq(int irq, void *dev_id)
+{
+	struct clock_event_device *evt = dev_id;
+
+	evt->event_handler(evt);
+
 	return IRQ_HANDLED;
 }
+
+static struct timer_of msc313_tick = {
+	.flags = TIMER_OF_IRQ | TIMER_OF_CLOCK | TIMER_OF_BASE,
+
+	.clkevt = {
+		.name = "msc313_tick",
+		.rating = 350,
+		.features = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT,
+		.set_state_shutdown = msc313e_timer_clkevt_shutdown,
+		.set_state_periodic = msc313e_timer_set_periodic,
+		.set_state_oneshot = msc313e_timer_clkevt_set_oneshot,
+		.tick_resume = msc313e_timer_clkevt_shutdown,
+		.set_next_event = msc313e_timer_clkevt_next_event,
+		.cpumask = cpu_possible_mask,
+	},
+
+	.of_irq = {
+		.handler = msc313e_timer_clkevt_irq,
+		.flags = IRQF_TIMER | IRQF_IRQPOLL,
+	},
+};
 
 static u64 msc313e_timer_read(struct clocksource *cs)
 {
 	struct msc313e_timer *timer = to_msc313e_timer(cs);
-	return (ioread16(timer->base + REG_COUNTER_LOW) | (ioread16(timer->base + REG_COUNTER_HIGH) << 16)) & cs->mask;
+	u64 result;
+
+	result = readw_relaxed(timer->oftimer.of_base.base + REG_COUNTER_LOW);
+	result |= readw_relaxed(timer->oftimer.of_base.base + REG_COUNTER_HIGH) << 16;
+
+	return result & cs->mask;
 }
 
 static int msc313e_timer_enable(struct clocksource *cs){
 	struct msc313e_timer *timer = to_msc313e_timer(cs);
-	regmap_field_write(timer->noten, 0);
+	u16 reg;
+
+	printk("enable\n");
+
+	reg = readw_relaxed(timer->oftimer.of_base.base + REG_CTRL);
+	reg |= CTRL_ENABLE;
+	writew_relaxed(reg, timer->oftimer.of_base.base + REG_CTRL);
+
 	return 0;
 };
 
 static void msc313e_timer_disable(struct clocksource *cs){
 	struct msc313e_timer *timer = to_msc313e_timer(cs);
-	regmap_field_write(timer->noten, 1);
+	u16 reg;
+
+	printk("disable\n");
+
+	reg = readw_relaxed(timer->oftimer.of_base.base + REG_CTRL);
+	reg &= ~CTRL_ENABLE;
+	writew_relaxed(reg, timer->oftimer.of_base.base + REG_CTRL);
 }
 
-static int msc313e_timer_probe(struct platform_device *pdev)
+static int __init msc313e_timer_probe(struct device_node *node)
 {
 	int ret;
 	struct msc313e_timer *timer;
-	struct resource *res;
-	int irq;
+	bool tick = of_property_read_bool(node, "mstar,schedclk");
+	bool schedclk = of_property_read_bool(node, "mstar,schedclk");
 
-	timer = devm_kzalloc(&pdev->dev, sizeof(*timer), GFP_KERNEL);
-	if (!timer)
-		return -ENOMEM;
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	timer->base = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(timer->base))
-		return PTR_ERR(timer->base);
-	
-	timer->regmap = devm_regmap_init_mmio(&pdev->dev, timer->base,
-			&msc313_timer_regmap_config);
-	if(IS_ERR(timer->regmap)){
-		dev_err(&pdev->dev, "failed to register regmap");
-		return PTR_ERR(timer->regmap);
+	if(tick){
+		ret = timer_of_init(node, &msc313_tick);
+		if(ret)
+			goto out;
+		clockevents_config_and_register(&msc313_tick.clkevt,
+				timer_of_rate(&msc313_tick),
+				TIMER_SYNC_TICKS, 0xffffffff);
+		goto out;
 	}
-	timer->noten = devm_regmap_field_alloc(&pdev->dev, timer->regmap, noten_field);
-	regmap_field_write(timer->noten, 1);
 
-	irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
-		if (!irq)
-			return -EINVAL;
+	timer = kzalloc(sizeof(*timer), GFP_KERNEL);
+	if (!timer){
+		ret = -ENOMEM;
+		goto out;
+	}
 
-	ret = devm_request_irq(&pdev->dev, irq, msc313e_timer_irq, IRQF_SHARED,
-			dev_name(&pdev->dev), timer);
+	timer->oftimer.flags = TIMER_OF_BASE | TIMER_OF_CLOCK;
+	timer->oftimer.of_irq.handler = msc313e_timer_clkevt_irq;
+	timer->oftimer.of_irq.flags = IRQF_TIMER | IRQF_IRQPOLL;
+	ret = timer_of_init(node, &timer->oftimer);
 	if (ret)
-		return ret;
+		goto out;
 
-	timer->clk = of_clk_get(pdev->dev.of_node, 0);
-	clk_prepare_enable(timer->clk);
-	
-	timer->clksrc.name = platform_get_device_id(pdev)->name;
+	timer->clksrc.name = node->name;
 	timer->clksrc.rating = 200;
 	timer->clksrc.read = msc313e_timer_read;
 	timer->clksrc.mask = CLOCKSOURCE_MASK(32);
 	timer->clksrc.flags = CLOCK_SOURCE_IS_CONTINUOUS;
 	timer->clksrc.enable =  msc313e_timer_enable;
 	timer->clksrc.disable =  msc313e_timer_disable;
-	
-	return clocksource_register_hz(&timer->clksrc, clk_get_rate(timer->clk));
+
+	ret = clocksource_register_hz(&timer->clksrc, timer_of_rate(&timer->oftimer));
+	if(ret)
+		goto out;
+
+out:
+	return ret;
 }
 
-static int msc313e_timer_remove(struct platform_device *pdev)
-{
-	return 0;
-}
-
-static struct platform_driver msc313e_timer_driver = {
-	.probe = msc313e_timer_probe,
-	.remove = msc313e_timer_remove,
-	.driver = {
-		   .name = DRIVER_NAME,
-		   .of_match_table = msc313e_timer_dt_ids,
-	},
-};
-
-module_platform_driver(msc313e_timer_driver);
-
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Mstar MSC313e PWM driver");
-MODULE_AUTHOR("Daniel Palmer <daniel@0x0f.com>");
+TIMER_OF_DECLARE(msc313, "mstar,msc313e-timer",
+		msc313e_timer_probe);
