wctrl/linux_msd781x

gpio: msc313: Add support for SSD201 and SSD202D
This adds GPIO support for the SSD201 and SSD202D chips.

Signed-off-by: Daniel Palmer <daniel@0x0f.com>

drivers/gpio/gpio-msc313.c
@@ -69,6 +69,83 @@
 #define MSC313_PINNAME_SPI0_DI		"spi0_di"
 #define MSC313_PINNAME_SPI0_DO		"spi0_do"
 
+/*
+ * pad names for "gpio" pins, only seen on the ssd20xd
+ * so far.
+ */
+
+#define SSD20XD_PINNAME_GPIO0		"gpio0"
+#define SSD20XD_PINNAME_GPIO1		"gpio1"
+#define SSD20XD_PINNAME_GPIO2		"gpio2"
+#define SSD20XD_PINNAME_GPIO3		"gpio3"
+#define SSD20XD_PINNAME_GPIO4		"gpio4"
+#define SSD20XD_PINNAME_GPIO5		"gpio5"
+#define SSD20XD_PINNAME_GPIO6		"gpio6"
+#define SSD20XD_PINNAME_GPIO7		"gpio7"
+#define SSD20XD_PINNAME_GPIO10		"gpio10"
+#define SSD20XD_PINNAME_GPIO11		"gpio11"
+#define SSD20XD_PINNAME_GPIO12		"gpio12"
+#define SSD20XD_PINNAME_GPIO13		"gpio13"
+#define SSD20XD_PINNAME_GPIO14		"gpio14"
+/*
+ * not wired up yet as the register offsets are
+ * unknown.
+ */
+#define SSD20XD_PINNAME_GPIO47		"gpio47"
+#define SSD20XD_PINNAME_GPIO48		"gpio48"
+#define SSD20XD_PINNAME_GPIO85		"gpio85"
+#define SSD20XD_PINNAME_GPIO86		"gpio86"
+#define SSD20XD_PINNAME_GPIO90		"gpio90"
+
+/*
+ * pad names for "ttl" pins, only seen on the ssd20xd
+ * so far
+ */
+
+#define SSD20XD_PINNAME_TTL0		"ttl0"
+#define SSD20XD_PINNAME_TTL1		"ttl1"
+#define SSD20XD_PINNAME_TTL2		"ttl2"
+#define SSD20XD_PINNAME_TTL3		"ttl3"
+#define SSD20XD_PINNAME_TTL4		"ttl4"
+#define SSD20XD_PINNAME_TTL5		"ttl5"
+#define SSD20XD_PINNAME_TTL6		"ttl6"
+#define SSD20XD_PINNAME_TTL7		"ttl7"
+#define SSD20XD_PINNAME_TTL8		"ttl8"
+#define SSD20XD_PINNAME_TTL9		"ttl9"
+#define SSD20XD_PINNAME_TTL10		"ttl10"
+#define SSD20XD_PINNAME_TTL11		"ttl11"
+#define SSD20XD_PINNAME_TTL12		"ttl12"
+#define SSD20XD_PINNAME_TTL13		"ttl13"
+#define SSD20XD_PINNAME_TTL14		"ttl14"
+#define SSD20XD_PINNAME_TTL15		"ttl15"
+#define SSD20XD_PINNAME_TTL16		"ttl16"
+#define SSD20XD_PINNAME_TTL17		"ttl17"
+#define SSD20XD_PINNAME_TTL18		"ttl18"
+#define SSD20XD_PINNAME_TTL19		"ttl19"
+#define SSD20XD_PINNAME_TTL20		"ttl20"
+#define SSD20XD_PINNAME_TTL21		"ttl21"
+#define SSD20XD_PINNAME_TTL22		"ttl22"
+#define SSD20XD_PINNAME_TTL23		"ttl23"
+#define SSD20XD_PINNAME_TTL24		"ttl24"
+#define SSD20XD_PINNAME_TTL25		"ttl25"
+#define SSD20XD_PINNAME_TTL26		"ttl26"
+#define SSD20XD_PINNAME_TTL27		"ttl27"
+
+#define SSD20XD_PINNAME_UART0_RX	"uart0_rx"
+#define SSD20XD_PINNAME_UART0_TX	"uart0_tx"
+
+#define SSD20XD_UART0_NAMES \
+	SSD20XD_PINNAME_UART0_RX, \
+	SSD20XD_PINNAME_UART0_TX
+
+#define OFF_UART_RX	0x60
+#define OFF_UART_TX	0x64
+
+#define SSD20XD_UART0_OFFSETS \
+	OFF_UART_RX, \
+	OFF_UART_TX
+
+
 #define FUART_NAMES			\
 	MSC313_PINNAME_FUART_RX,	\
 	MSC313_PINNAME_FUART_TX,	\
@@ -221,6 +298,160 @@ static const unsigned int msc313_offsets[] = {
 };
 
 MSC313_GPIO_CHIPDATA(msc313);
+
+/* ssd20[1-2]d */
+
+#define SSD20XD_GPIO_NAMES SSD20XD_PINNAME_GPIO0, \
+			   SSD20XD_PINNAME_GPIO1, \
+			   SSD20XD_PINNAME_GPIO2, \
+			   SSD20XD_PINNAME_GPIO3, \
+			   SSD20XD_PINNAME_GPIO4, \
+			   SSD20XD_PINNAME_GPIO5, \
+			   SSD20XD_PINNAME_GPIO6, \
+			   SSD20XD_PINNAME_GPIO7, \
+			   SSD20XD_PINNAME_GPIO10, \
+			   SSD20XD_PINNAME_GPIO11, \
+			   SSD20XD_PINNAME_GPIO12, \
+			   SSD20XD_PINNAME_GPIO13, \
+			   SSD20XD_PINNAME_GPIO14, \
+			   SSD20XD_PINNAME_GPIO85, \
+			   SSD20XD_PINNAME_GPIO90
+
+#define SSD20XD_GPIO_OFF_GPIO0 0x0
+#define SSD20XD_GPIO_OFF_GPIO1 0x4
+#define SSD20XD_GPIO_OFF_GPIO2 0x8
+#define SSD20XD_GPIO_OFF_GPIO3 0xc
+#define SSD20XD_GPIO_OFF_GPIO4 0x10
+#define SSD20XD_GPIO_OFF_GPIO5 0x14
+#define SSD20XD_GPIO_OFF_GPIO6 0x18
+#define SSD20XD_GPIO_OFF_GPIO7 0x1c
+#define SSD20XD_GPIO_OFF_GPIO10 0x28
+#define SSD20XD_GPIO_OFF_GPIO11 0x2c
+#define SSD20XD_GPIO_OFF_GPIO12 0x30
+#define SSD20XD_GPIO_OFF_GPIO13 0x34
+#define SSD20XD_GPIO_OFF_GPIO14 0x38
+#define SSD20XD_GPIO_OFF_GPIO85 0x100
+//#define SSD20XD_GPIO_OFF_GPIO86 0x104
+#define SSD20XD_GPIO_OFF_GPIO90 0x104
+
+#define SSD20XD_GPIO_OFFSETS SSD20XD_GPIO_OFF_GPIO0, \
+			     SSD20XD_GPIO_OFF_GPIO1, \
+			     SSD20XD_GPIO_OFF_GPIO2, \
+			     SSD20XD_GPIO_OFF_GPIO3, \
+			     SSD20XD_GPIO_OFF_GPIO4, \
+			     SSD20XD_GPIO_OFF_GPIO5, \
+			     SSD20XD_GPIO_OFF_GPIO6, \
+			     SSD20XD_GPIO_OFF_GPIO7, \
+			     SSD20XD_GPIO_OFF_GPIO10, \
+			     SSD20XD_GPIO_OFF_GPIO11, \
+			     SSD20XD_GPIO_OFF_GPIO12, \
+			     SSD20XD_GPIO_OFF_GPIO13, \
+			     SSD20XD_GPIO_OFF_GPIO14, \
+			     SSD20XD_GPIO_OFF_GPIO85, \
+			     SSD20XD_GPIO_OFF_GPIO90
+
+#define SSD20XD_TTL_PINNAMES SSD20XD_PINNAME_TTL0, \
+			     SSD20XD_PINNAME_TTL1, \
+			     SSD20XD_PINNAME_TTL2, \
+			     SSD20XD_PINNAME_TTL3, \
+			     SSD20XD_PINNAME_TTL4, \
+			     SSD20XD_PINNAME_TTL5, \
+			     SSD20XD_PINNAME_TTL6, \
+			     SSD20XD_PINNAME_TTL7, \
+			     SSD20XD_PINNAME_TTL8, \
+			     SSD20XD_PINNAME_TTL9, \
+			     SSD20XD_PINNAME_TTL10, \
+			     SSD20XD_PINNAME_TTL11, \
+			     SSD20XD_PINNAME_TTL12, \
+			     SSD20XD_PINNAME_TTL13, \
+			     SSD20XD_PINNAME_TTL14, \
+			     SSD20XD_PINNAME_TTL15, \
+			     SSD20XD_PINNAME_TTL16, \
+			     SSD20XD_PINNAME_TTL17, \
+			     SSD20XD_PINNAME_TTL18, \
+			     SSD20XD_PINNAME_TTL19, \
+			     SSD20XD_PINNAME_TTL20, \
+			     SSD20XD_PINNAME_TTL21, \
+			     SSD20XD_PINNAME_TTL22, \
+			     SSD20XD_PINNAME_TTL23, \
+			     SSD20XD_PINNAME_TTL24, \
+			     SSD20XD_PINNAME_TTL25, \
+			     SSD20XD_PINNAME_TTL26, \
+			     SSD20XD_PINNAME_TTL27
+
+#define SSD20XD_TTL_OFFSET_TTL0		0x80
+#define SSD20XD_TTL_OFFSET_TTL1		0x84
+#define SSD20XD_TTL_OFFSET_TTL2		0x88
+#define SSD20XD_TTL_OFFSET_TTL3		0x8c
+#define SSD20XD_TTL_OFFSET_TTL4		0x90
+#define SSD20XD_TTL_OFFSET_TTL5		0x94
+#define SSD20XD_TTL_OFFSET_TTL6		0x98
+#define SSD20XD_TTL_OFFSET_TTL7		0x9c
+#define SSD20XD_TTL_OFFSET_TTL8		0x100
+#define SSD20XD_TTL_OFFSET_TTL9		0x104
+#define SSD20XD_TTL_OFFSET_TTL10	0x108
+#define SSD20XD_TTL_OFFSET_TTL11	0x10c
+#define SSD20XD_TTL_OFFSET_TTL12	0x110
+#define SSD20XD_TTL_OFFSET_TTL13	0x114
+#define SSD20XD_TTL_OFFSET_TTL14	0x118
+#define SSD20XD_TTL_OFFSET_TTL15	0x11c
+#define SSD20XD_TTL_OFFSET_TTL16	0x120
+#define SSD20XD_TTL_OFFSET_TTL17	0x124
+#define SSD20XD_TTL_OFFSET_TTL18	0x128
+#define SSD20XD_TTL_OFFSET_TTL19	0x12c
+#define SSD20XD_TTL_OFFSET_TTL20	0x130
+#define SSD20XD_TTL_OFFSET_TTL21	0x134
+#define SSD20XD_TTL_OFFSET_TTL22	0x138
+#define SSD20XD_TTL_OFFSET_TTL23	0x13c
+#define SSD20XD_TTL_OFFSET_TTL24	0x140
+#define SSD20XD_TTL_OFFSET_TTL25	0x144
+#define SSD20XD_TTL_OFFSET_TTL26	0x148
+#define SSD20XD_TTL_OFFSET_TTL27	0x14c
+
+#define SSD20XD_TTL_OFFSETS SSD20XD_TTL_OFFSET_TTL0, \
+			    SSD20XD_TTL_OFFSET_TTL1, \
+			    SSD20XD_TTL_OFFSET_TTL2, \
+			    SSD20XD_TTL_OFFSET_TTL3, \
+			    SSD20XD_TTL_OFFSET_TTL4, \
+			    SSD20XD_TTL_OFFSET_TTL5, \
+			    SSD20XD_TTL_OFFSET_TTL6, \
+			    SSD20XD_TTL_OFFSET_TTL7, \
+			    SSD20XD_TTL_OFFSET_TTL8, \
+			    SSD20XD_TTL_OFFSET_TTL9, \
+			    SSD20XD_TTL_OFFSET_TTL10, \
+			    SSD20XD_TTL_OFFSET_TTL11, \
+			    SSD20XD_TTL_OFFSET_TTL12, \
+			    SSD20XD_TTL_OFFSET_TTL13, \
+			    SSD20XD_TTL_OFFSET_TTL14, \
+			    SSD20XD_TTL_OFFSET_TTL15, \
+			    SSD20XD_TTL_OFFSET_TTL16, \
+			    SSD20XD_TTL_OFFSET_TTL17, \
+			    SSD20XD_TTL_OFFSET_TTL18, \
+			    SSD20XD_TTL_OFFSET_TTL19, \
+			    SSD20XD_TTL_OFFSET_TTL20, \
+			    SSD20XD_TTL_OFFSET_TTL21, \
+			    SSD20XD_TTL_OFFSET_TTL22, \
+			    SSD20XD_TTL_OFFSET_TTL23, \
+			    SSD20XD_TTL_OFFSET_TTL24, \
+			    SSD20XD_TTL_OFFSET_TTL25, \
+			    SSD20XD_TTL_OFFSET_TTL26, \
+			    SSD20XD_TTL_OFFSET_TTL27
+
+static const char * const ssd20xd_names[] = {
+	FUART_NAMES,
+	SSD20XD_GPIO_NAMES,
+	SSD20XD_TTL_PINNAMES,
+	SSD20XD_UART0_NAMES,
+};
+
+static const unsigned int ssd20xd_offsets[] = {
+	FUART_OFFSETS,
+	SSD20XD_GPIO_OFFSETS,
+	SSD20XD_TTL_OFFSETS,
+	SSD20XD_UART0_OFFSETS,
+};
+
+MSC313_GPIO_CHIPDATA(ssd20xd);
 #endif
 
 struct msc313_gpio {
@@ -412,6 +643,10 @@ static const struct of_device_id msc313_gpio_of_match[] = {
 	{
 		.compatible = "mstar,msc313-gpio",
 		.data = &msc313_data,
+	},
+	{
+		.compatible = "sstar,ssd20xd-gpio",
+		.data = &ssd20xd_data,
 	},
 #endif
 	{ }

fifteenhex/linux

drivers/gpio/gpio-msc313.c
@@ -69,6 +69,83 @@
 #define MSC313_PINNAME_SPI0_DI		"spi0_di"
 #define MSC313_PINNAME_SPI0_DO		"spi0_do"
 
+/*
+ * pad names for "gpio" pins, only seen on the ssd20xd
+ * so far.
+ */
+
+#define SSD20XD_PINNAME_GPIO0		"gpio0"
+#define SSD20XD_PINNAME_GPIO1		"gpio1"
+#define SSD20XD_PINNAME_GPIO2		"gpio2"
+#define SSD20XD_PINNAME_GPIO3		"gpio3"
+#define SSD20XD_PINNAME_GPIO4		"gpio4"
+#define SSD20XD_PINNAME_GPIO5		"gpio5"
+#define SSD20XD_PINNAME_GPIO6		"gpio6"
+#define SSD20XD_PINNAME_GPIO7		"gpio7"
+#define SSD20XD_PINNAME_GPIO10		"gpio10"
+#define SSD20XD_PINNAME_GPIO11		"gpio11"
+#define SSD20XD_PINNAME_GPIO12		"gpio12"
+#define SSD20XD_PINNAME_GPIO13		"gpio13"
+#define SSD20XD_PINNAME_GPIO14		"gpio14"
+/*
+ * not wired up yet as the register offsets are
+ * unknown.
+ */
+#define SSD20XD_PINNAME_GPIO47		"gpio47"
+#define SSD20XD_PINNAME_GPIO48		"gpio48"
+#define SSD20XD_PINNAME_GPIO85		"gpio85"
+#define SSD20XD_PINNAME_GPIO86		"gpio86"
+#define SSD20XD_PINNAME_GPIO90		"gpio90"
+
+/*
+ * pad names for "ttl" pins, only seen on the ssd20xd
+ * so far
+ */
+
+#define SSD20XD_PINNAME_TTL0		"ttl0"
+#define SSD20XD_PINNAME_TTL1		"ttl1"
+#define SSD20XD_PINNAME_TTL2		"ttl2"
+#define SSD20XD_PINNAME_TTL3		"ttl3"
+#define SSD20XD_PINNAME_TTL4		"ttl4"
+#define SSD20XD_PINNAME_TTL5		"ttl5"
+#define SSD20XD_PINNAME_TTL6		"ttl6"
+#define SSD20XD_PINNAME_TTL7		"ttl7"
+#define SSD20XD_PINNAME_TTL8		"ttl8"
+#define SSD20XD_PINNAME_TTL9		"ttl9"
+#define SSD20XD_PINNAME_TTL10		"ttl10"
+#define SSD20XD_PINNAME_TTL11		"ttl11"
+#define SSD20XD_PINNAME_TTL12		"ttl12"
+#define SSD20XD_PINNAME_TTL13		"ttl13"
+#define SSD20XD_PINNAME_TTL14		"ttl14"
+#define SSD20XD_PINNAME_TTL15		"ttl15"
+#define SSD20XD_PINNAME_TTL16		"ttl16"
+#define SSD20XD_PINNAME_TTL17		"ttl17"
+#define SSD20XD_PINNAME_TTL18		"ttl18"
+#define SSD20XD_PINNAME_TTL19		"ttl19"
+#define SSD20XD_PINNAME_TTL20		"ttl20"
+#define SSD20XD_PINNAME_TTL21		"ttl21"
+#define SSD20XD_PINNAME_TTL22		"ttl22"
+#define SSD20XD_PINNAME_TTL23		"ttl23"
+#define SSD20XD_PINNAME_TTL24		"ttl24"
+#define SSD20XD_PINNAME_TTL25		"ttl25"
+#define SSD20XD_PINNAME_TTL26		"ttl26"
+#define SSD20XD_PINNAME_TTL27		"ttl27"
+
+#define SSD20XD_PINNAME_UART0_RX	"uart0_rx"
+#define SSD20XD_PINNAME_UART0_TX	"uart0_tx"
+
+#define SSD20XD_UART0_NAMES \
+	SSD20XD_PINNAME_UART0_RX, \
+	SSD20XD_PINNAME_UART0_TX
+
+#define OFF_UART_RX	0x60
+#define OFF_UART_TX	0x64
+
+#define SSD20XD_UART0_OFFSETS \
+	OFF_UART_RX, \
+	OFF_UART_TX
+
+
 #define FUART_NAMES			\
 	MSC313_PINNAME_FUART_RX,	\
 	MSC313_PINNAME_FUART_TX,	\
@@ -221,6 +298,160 @@ static const unsigned int msc313_offsets[] = {
 };
 
 MSC313_GPIO_CHIPDATA(msc313);
+
+/* ssd20[1-2]d */
+
+#define SSD20XD_GPIO_NAMES SSD20XD_PINNAME_GPIO0, \
+			   SSD20XD_PINNAME_GPIO1, \
+			   SSD20XD_PINNAME_GPIO2, \
+			   SSD20XD_PINNAME_GPIO3, \
+			   SSD20XD_PINNAME_GPIO4, \
+			   SSD20XD_PINNAME_GPIO5, \
+			   SSD20XD_PINNAME_GPIO6, \
+			   SSD20XD_PINNAME_GPIO7, \
+			   SSD20XD_PINNAME_GPIO10, \
+			   SSD20XD_PINNAME_GPIO11, \
+			   SSD20XD_PINNAME_GPIO12, \
+			   SSD20XD_PINNAME_GPIO13, \
+			   SSD20XD_PINNAME_GPIO14, \
+			   SSD20XD_PINNAME_GPIO85, \
+			   SSD20XD_PINNAME_GPIO90
+
+#define SSD20XD_GPIO_OFF_GPIO0 0x0
+#define SSD20XD_GPIO_OFF_GPIO1 0x4
+#define SSD20XD_GPIO_OFF_GPIO2 0x8
+#define SSD20XD_GPIO_OFF_GPIO3 0xc
+#define SSD20XD_GPIO_OFF_GPIO4 0x10
+#define SSD20XD_GPIO_OFF_GPIO5 0x14
+#define SSD20XD_GPIO_OFF_GPIO6 0x18
+#define SSD20XD_GPIO_OFF_GPIO7 0x1c
+#define SSD20XD_GPIO_OFF_GPIO10 0x28
+#define SSD20XD_GPIO_OFF_GPIO11 0x2c
+#define SSD20XD_GPIO_OFF_GPIO12 0x30
+#define SSD20XD_GPIO_OFF_GPIO13 0x34
+#define SSD20XD_GPIO_OFF_GPIO14 0x38
+#define SSD20XD_GPIO_OFF_GPIO85 0x100
+//#define SSD20XD_GPIO_OFF_GPIO86 0x104
+#define SSD20XD_GPIO_OFF_GPIO90 0x104
+
+#define SSD20XD_GPIO_OFFSETS SSD20XD_GPIO_OFF_GPIO0, \
+			     SSD20XD_GPIO_OFF_GPIO1, \
+			     SSD20XD_GPIO_OFF_GPIO2, \
+			     SSD20XD_GPIO_OFF_GPIO3, \
+			     SSD20XD_GPIO_OFF_GPIO4, \
+			     SSD20XD_GPIO_OFF_GPIO5, \
+			     SSD20XD_GPIO_OFF_GPIO6, \
+			     SSD20XD_GPIO_OFF_GPIO7, \
+			     SSD20XD_GPIO_OFF_GPIO10, \
+			     SSD20XD_GPIO_OFF_GPIO11, \
+			     SSD20XD_GPIO_OFF_GPIO12, \
+			     SSD20XD_GPIO_OFF_GPIO13, \
+			     SSD20XD_GPIO_OFF_GPIO14, \
+			     SSD20XD_GPIO_OFF_GPIO85, \
+			     SSD20XD_GPIO_OFF_GPIO90
+
+#define SSD20XD_TTL_PINNAMES SSD20XD_PINNAME_TTL0, \
+			     SSD20XD_PINNAME_TTL1, \
+			     SSD20XD_PINNAME_TTL2, \
+			     SSD20XD_PINNAME_TTL3, \
+			     SSD20XD_PINNAME_TTL4, \
+			     SSD20XD_PINNAME_TTL5, \
+			     SSD20XD_PINNAME_TTL6, \
+			     SSD20XD_PINNAME_TTL7, \
+			     SSD20XD_PINNAME_TTL8, \
+			     SSD20XD_PINNAME_TTL9, \
+			     SSD20XD_PINNAME_TTL10, \
+			     SSD20XD_PINNAME_TTL11, \
+			     SSD20XD_PINNAME_TTL12, \
+			     SSD20XD_PINNAME_TTL13, \
+			     SSD20XD_PINNAME_TTL14, \
+			     SSD20XD_PINNAME_TTL15, \
+			     SSD20XD_PINNAME_TTL16, \
+			     SSD20XD_PINNAME_TTL17, \
+			     SSD20XD_PINNAME_TTL18, \
+			     SSD20XD_PINNAME_TTL19, \
+			     SSD20XD_PINNAME_TTL20, \
+			     SSD20XD_PINNAME_TTL21, \
+			     SSD20XD_PINNAME_TTL22, \
+			     SSD20XD_PINNAME_TTL23, \
+			     SSD20XD_PINNAME_TTL24, \
+			     SSD20XD_PINNAME_TTL25, \
+			     SSD20XD_PINNAME_TTL26, \
+			     SSD20XD_PINNAME_TTL27
+
+#define SSD20XD_TTL_OFFSET_TTL0		0x80
+#define SSD20XD_TTL_OFFSET_TTL1		0x84
+#define SSD20XD_TTL_OFFSET_TTL2		0x88
+#define SSD20XD_TTL_OFFSET_TTL3		0x8c
+#define SSD20XD_TTL_OFFSET_TTL4		0x90
+#define SSD20XD_TTL_OFFSET_TTL5		0x94
+#define SSD20XD_TTL_OFFSET_TTL6		0x98
+#define SSD20XD_TTL_OFFSET_TTL7		0x9c
+#define SSD20XD_TTL_OFFSET_TTL8		0xa0
+#define SSD20XD_TTL_OFFSET_TTL9		0xa4
+#define SSD20XD_TTL_OFFSET_TTL10	0xa8
+#define SSD20XD_TTL_OFFSET_TTL11	0xac
+#define SSD20XD_TTL_OFFSET_TTL12	0xb0
+#define SSD20XD_TTL_OFFSET_TTL13	0xb4
+#define SSD20XD_TTL_OFFSET_TTL14	0xb8
+#define SSD20XD_TTL_OFFSET_TTL15	0xbc
+#define SSD20XD_TTL_OFFSET_TTL16	0xc0
+#define SSD20XD_TTL_OFFSET_TTL17	0xc4
+#define SSD20XD_TTL_OFFSET_TTL18	0xc8
+#define SSD20XD_TTL_OFFSET_TTL19	0xcc
+#define SSD20XD_TTL_OFFSET_TTL20	0xd0
+#define SSD20XD_TTL_OFFSET_TTL21	0xd4
+#define SSD20XD_TTL_OFFSET_TTL22	0xd8
+#define SSD20XD_TTL_OFFSET_TTL23	0xdc
+#define SSD20XD_TTL_OFFSET_TTL24	0xe0
+#define SSD20XD_TTL_OFFSET_TTL25	0xe4
+#define SSD20XD_TTL_OFFSET_TTL26	0xe8
+#define SSD20XD_TTL_OFFSET_TTL27	0xec
+
+#define SSD20XD_TTL_OFFSETS SSD20XD_TTL_OFFSET_TTL0, \
+			    SSD20XD_TTL_OFFSET_TTL1, \
+			    SSD20XD_TTL_OFFSET_TTL2, \
+			    SSD20XD_TTL_OFFSET_TTL3, \
+			    SSD20XD_TTL_OFFSET_TTL4, \
+			    SSD20XD_TTL_OFFSET_TTL5, \
+			    SSD20XD_TTL_OFFSET_TTL6, \
+			    SSD20XD_TTL_OFFSET_TTL7, \
+			    SSD20XD_TTL_OFFSET_TTL8, \
+			    SSD20XD_TTL_OFFSET_TTL9, \
+			    SSD20XD_TTL_OFFSET_TTL10, \
+			    SSD20XD_TTL_OFFSET_TTL11, \
+			    SSD20XD_TTL_OFFSET_TTL12, \
+			    SSD20XD_TTL_OFFSET_TTL13, \
+			    SSD20XD_TTL_OFFSET_TTL14, \
+			    SSD20XD_TTL_OFFSET_TTL15, \
+			    SSD20XD_TTL_OFFSET_TTL16, \
+			    SSD20XD_TTL_OFFSET_TTL17, \
+			    SSD20XD_TTL_OFFSET_TTL18, \
+			    SSD20XD_TTL_OFFSET_TTL19, \
+			    SSD20XD_TTL_OFFSET_TTL20, \
+			    SSD20XD_TTL_OFFSET_TTL21, \
+			    SSD20XD_TTL_OFFSET_TTL22, \
+			    SSD20XD_TTL_OFFSET_TTL23, \
+			    SSD20XD_TTL_OFFSET_TTL24, \
+			    SSD20XD_TTL_OFFSET_TTL25, \
+			    SSD20XD_TTL_OFFSET_TTL26, \
+			    SSD20XD_TTL_OFFSET_TTL27
+
+static const char * const ssd20xd_names[] = {
+	FUART_NAMES,
+	SSD20XD_GPIO_NAMES,
+	SSD20XD_TTL_PINNAMES,
+	SSD20XD_UART0_NAMES,
+};
+
+static const unsigned int ssd20xd_offsets[] = {
+	FUART_OFFSETS,
+	SSD20XD_GPIO_OFFSETS,
+	SSD20XD_TTL_OFFSETS,
+	SSD20XD_UART0_OFFSETS,
+};
+
+MSC313_GPIO_CHIPDATA(ssd20xd);
 #endif
 
 struct msc313_gpio {
@@ -412,6 +643,10 @@ static const struct of_device_id msc313_gpio_of_match[] = {
 	{
 		.compatible = "mstar,msc313-gpio",
 		.data = &msc313_data,
+	},
+	{
+		.compatible = "sstar,ssd20xd-gpio",
+		.data = &ssd20xd_data,
 	},
 #endif
 	{ }