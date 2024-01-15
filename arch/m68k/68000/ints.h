f0c0677900e418967acea484c93f5f8854192b6d
Daniel Palmer <daniel@0x0f.com>
2024-01-15 23:18:02 +0900

    ints

--- arch/m68k/68000/ints.h
+++ arch/m68k/68000/ints.h
@@ -4,4 +4,7 @@
 
 struct pt_regs;
 
+asmlinkage void bad_trap(struct pt_regs *fp);
 asmlinkage void process_int(struct pt_regs *fp);
+asmlinkage void process_int_oops(struct pt_regs *fp);
+asmlinkage void process_int_autovec(struct pt_regs *fp);
