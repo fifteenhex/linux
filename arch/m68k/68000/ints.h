02d81fa683e2def444989cc9a2b8443a42afcb4b
Daniel Palmer <daniel@0x0f.com>
2024-02-11 11:32:11 +0900

    68 stuff, db ints, split

--- arch/m68k/68000/ints.h
+++ arch/m68k/68000/ints.h
@@ -5,6 +5,6 @@
 struct pt_regs;
 
 asmlinkage void bad_trap(struct pt_regs *fp);
-asmlinkage void process_int(struct pt_regs *fp);
 asmlinkage void process_int_oops(struct pt_regs *fp);
 asmlinkage void process_int_autovec(struct pt_regs *fp);
+asmlinkage void process_int_user(struct pt_regs *fp);
