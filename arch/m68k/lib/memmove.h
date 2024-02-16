da6ec5c5e41a64fb8c36e44435bad182c8f85e56
Daniel Palmer <daniel@0x0f.com>
2024-02-16 21:26:27 +0900

    the great make memcpy less shit

--- arch/m68k/lib/memmove.h
+++ arch/m68k/lib/memmove.h
@@ -5,6 +5,24 @@
 #ifndef ARCH_M68K_LIB_MEM_H_
 #define ARCH_M68K_LIB_MEM_H_
 
+static inline void copy32_l(void *dest, const void *src) __attribute__((always_inline));
+static inline void copy32_l(void *dest, const void *src)
+{
+	asm volatile (  "movem.l (%[from])+, %%d0-%%d7\n"
+			"movem.l %%d0-%%d7, (%[to])\n"
+			:
+			: [from] "a" (src), [to] "a" (dest)
+			: "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7");
+}
+
+/*
+ * Alignment of src and dest differ so it's impossible
+ * to do a few byte or word operations at first then
+ * switch to longs as one of the pointers will still
+ * be misaligned.
+ */
+#define WONKY(_dst, _src) ((((long) _dst) & 1) + (((long) _src) & 1) == 1)
+
 static inline void forward_aligned_src(void *dest, const void *src, size_t n) __attribute__((always_inline));
 static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 {
@@ -26,6 +44,13 @@ static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 		src = ssrc;
 		n -= 2;
 	}
+
+	for(; n >= 32; n -= 32) {
+		copy32_l(dest, src);
+		dest += 32;
+		src += 32;
+	}
+
 	temp = n >> 2;
 	if (temp) {
 		long *ldest = dest;
