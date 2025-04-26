b1ac18df364a8fc40b992ceef62b3ecbb19b4874
Daniel Palmer <daniel@0x0f.com>
2025-04-26 13:04:48 +0900

    memcpy

--- arch/m68k/lib/memmove.h
+++ arch/m68k/lib/memmove.h
@@ -6,14 +6,24 @@
 #define ARCH_M68K_LIB_MEM_H_
 
 static inline void copy32_l(void *dest, const void *src) __attribute__((always_inline));
+#if defined(CONFIG_M68040)
 static inline void copy32_l(void *dest, const void *src)
 {
-	asm volatile (  "movem.l (%[from])+, %%d0-%%d7\n"
+	asm volatile ("move16 (%[from])+, (%[to])+\n"
+		      "move16 (%[from])+, (%[to])+\n"
+			:
+			: [from] "a" (src), [to] "a" (dest));
+}
+#else
+static inline void copy32_l(void *dest, const void *src)
+{
+	asm volatile (  "movem.l (%[from]), %%d0-%%d7\n"
 			"movem.l %%d0-%%d7, (%[to])\n"
 			:
 			: [from] "a" (src), [to] "a" (dest)
 			: "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7");
 }
+#endif
 
 /*
  * Alignment of src and dest differ so it's impossible
@@ -36,6 +46,7 @@ static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 		src = csrc;
 		n--;
 	}
+
 	if (n > 2 && (long)dest & 2) {
 		short *sdest = dest;
 		const short *ssrc = src;
@@ -45,11 +56,13 @@ static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 		n -= 2;
 	}
 
+#if 1
 	for(; n >= 32; n -= 32) {
 		copy32_l(dest, src);
 		dest += 32;
 		src += 32;
 	}
+#endif
 
 	temp = n >> 2;
 	if (temp) {
@@ -62,6 +75,7 @@ static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 		dest = ldest;
 		src = lsrc;
 	}
+
 	if (n & 2) {
 		short *sdest = dest;
 		const short *ssrc = src;
@@ -69,6 +83,7 @@ static inline void forward_aligned_src(void *dest, const void *src, size_t n)
 		dest = sdest;
 		src = ssrc;
 	}
+
 	if (n & 1) {
 		char *cdest = dest;
 		const char *csrc = src;
