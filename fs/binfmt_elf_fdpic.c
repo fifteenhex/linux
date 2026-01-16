8571e64cc1d5ca6d4b6d3540d96b4ed16a76f6e0
Daniel Palmer <daniel@thingy.jp>
2026-01-16 23:41:35 +0900

    fdpic debug

--- fs/binfmt_elf_fdpic.c
+++ fs/binfmt_elf_fdpic.c
@@ -42,13 +42,13 @@
 
 typedef char *elf_caddr_t;
 
-#if 0
+#if 1
 #define kdebug(fmt, ...) printk("FDPIC "fmt"\n" ,##__VA_ARGS__ )
 #else
 #define kdebug(fmt, ...) do {} while(0)
 #endif
 
-#if 0
+#if 1
 #define kdcore(fmt, ...) printk("FDPIC "fmt"\n" ,##__VA_ARGS__ )
 #else
 #define kdcore(fmt, ...) do {} while(0)
