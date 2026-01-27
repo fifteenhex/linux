5a929bff382cca8fe98a9ee4d2108aa48ba0846a
Daniel Palmer <daniel@thingy.jp>
2026-01-28 06:43:07 +0900

    tools/nolibc: ppc: Add relocation support
    Add relocation support for 32bit and 64bit ppc.
    
    Signed-off-by: Daniel Palmer <daniel@thingy.jp>

--- tools/include/nolibc/arch-powerpc.h
+++ tools/include/nolibc/arch-powerpc.h
@@ -7,11 +7,19 @@
 #ifndef _NOLIBC_ARCH_POWERPC_H
 #define _NOLIBC_ARCH_POWERPC_H
 
+#include "elf.h"
+
+#define _NOLIBC_ARCH_HAS_RELOC
+#ifndef __powerpc64__
+#define _NOLIBC_ARCH_ELF32
+#endif
+
 #include <linux/unistd.h>
 
 #include "compiler.h"
 #include "crt.h"
 #include "std.h"
+#include "reloc.h"
 
 /* Syscalls for PowerPC :
  *   - stack is 16-byte aligned
@@ -187,6 +195,22 @@
 #endif /* !__powerpc64__ */
 
 #ifndef NOLIBC_NO_RUNTIME
+
+#ifdef NOLIBC_WANT_RELOC
+static __inline__ int __relocate_rela(unsigned long base, _nolibc_elf_rela *entry)
+{
+	switch (_nolibc_elf_r_type(entry->r_info)) {
+	case R_PPC_RELATIVE:
+		__relocate_rela_relative(base, entry);
+		break;
+	default:
+		return -1;
+	}
+
+	return 0;
+}
+#endif /* NOLIBC_WANT_RELOC */
+
 /* startup code */
 void __attribute__((weak, noreturn)) __nolibc_entrypoint __nolibc_no_stack_protector _start(void)
 {
